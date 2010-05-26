2cd5b58c767c60b7ada52d485dc7490fe62d2893
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-05-26 14:52:47 -0700

    clean up test messages

--- test/has_association_test.rb
+++ test/has_association_test.rb
@@ -1,6 +1,6 @@
 require File.join(File.dirname(__FILE__),'teststrap')
 
-context "has_association" do
+context "has_association macro" do
   setup do
     test = mock_model do
       embeds_many :things
@@ -9,23 +9,23 @@ context "has_association" do
     end
   end
 
-  asserts "the macro passes when the association options are specified for a has_many" do
+  asserts "passes when the association options are specified for a has_many" do
     RiotMongoid::HasAssociationAssertion.new.evaluate(topic, :embeds_many, :things).first
   end.equals(:pass)
 
-  asserts "the macro passes when the association options are specified for a belongs_to" do
+  asserts "passes when the association options are specified for a belongs_to" do
     RiotMongoid::HasAssociationAssertion.new.evaluate(topic, :embedded_in, :another_thing, :inverse_of => :word).first
   end.equals(:pass)
 
-  asserts "the macro pass message is useful" do
+  asserts "returns useful message" do
     RiotMongoid::HasAssociationAssertion.new.evaluate(topic, :embedded_in, :another_thing, :inverse_of => :word).last
   end.matches(/has 'embedded_in' association 'another_thing' with options \{:inverse_of=>:word\}/)
 
-  asserts "the macro passes when the association options are specified for a has_many_related" do
+  asserts "passes when the association options are specified for a has_many_related" do
     RiotMongoid::HasAssociationAssertion.new.evaluate(topic, :has_many_related, :relations).first
   end.equals(:pass)
 
-  asserts "the macro fails when no association name is specified" do
+  asserts "fails when no association name is specified" do
     RiotMongoid::HasAssociationAssertion.new.evaluate(topic, :has_many).first
   end.equals(:fail)
 
