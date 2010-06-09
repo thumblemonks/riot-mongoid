574202d9f80d0ce5fe4ca0136004cbe963756842
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-06-09 08:13:13 +0800

    clean up has_key macro

--- lib/riot-mongoid/has_key.rb
+++ lib/riot-mongoid/has_key.rb
@@ -3,11 +3,10 @@ module RiotMongoid
     register :has_key
 
     def evaluate(model, *key_macro_info)
-      valid = key_macro_info == model.primary_key
-      case
-      when key_macro_info.nil?
+      if key_macro_info.nil?
         fail("keys must be specified with this assertion macro")
       else
+        valid = key_macro_info == model.primary_key
         key = key_macro_info.join('-')
         valid ? pass("#{model} has key #{key}") : fail("expected #{model} to have key #{key}")
       end
