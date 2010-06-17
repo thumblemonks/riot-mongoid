9fd559f2115ea6c9d5caa94ac1018be7f45be801
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-06-17 11:58:24 -0700

    fix has_association association options check

--- test/has_association_test.rb
+++ test/has_association_test.rb
@@ -6,6 +6,7 @@ context "has_association macro" do
       embeds_many :things
       embedded_in :another_thing, :inverse_of => :word
       has_many_related :relations
+      has_many_related :reporters, :klass => 'Person'
     end
   end
 
@@ -21,10 +22,14 @@ context "has_association macro" do
     RiotMongoid::HasAssociationAssertion.new.evaluate(topic, :embedded_in, :another_thing, :inverse_of => :word).last
   end.matches(/has 'embedded_in' association 'another_thing' with options \{:inverse_of=>:word\}/)
 
-  asserts "passes when the association options are specified for a has_many_related" do
+  asserts "passes when the no association options are specified for a has_many_related" do
     RiotMongoid::HasAssociationAssertion.new.evaluate(topic, :has_many_related, :relations).first
   end.equals(:pass)
 
+  asserts "passes when the association options are specified for a has_many_related" do
+    RiotMongoid::HasAssociationAssertion.new.evaluate(topic, :has_many_related, :reporters, :klass => 'Person').first
+  end.equals(:pass)
+  
   asserts "fails when no association name is specified" do
     RiotMongoid::HasAssociationAssertion.new.evaluate(topic, :has_many).first
   end.equals(:fail)
